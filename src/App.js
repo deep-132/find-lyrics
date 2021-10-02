2a398247dddc97395200bd6e2904349976326ea8
Deepansh Maheshwari <deepmaheshwari23@gmail.com>
2021-10-02 19:09:46 +0530

    added context.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "./context";
 
-function App() {
-  return (
-    <div className="App">
-      hi
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/context.js
@@ -0,0 +1,74 @@
+// create a provider,wrap everything in this component
+
+import React, { Component } from "react";
+import axios from "axios";
+
+const Context = React.createContext();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    // the response we get in the search component will be sent as payload
+    case "SEARCH_TRACKS":
+      return {
+        ...state,
+        searchingFinished: true,
+        track_list: action.payload,
+        heading: "Search Results"
+      };
+
+    case "BEGIN_SEARCH":
+      return {
+        ...state,
+        searchingFinished: false,
+        track_list: [],
+      };
+
+
+    default:
+      return state;
+  }
+};
+
+export class Provider extends Component {
+  // this state gets passed down to any consumer
+  state = {
+    searchingFinished: false,
+    track_list: [],
+    heading: "Top 10 Tracks",
+    // this will allow us to have a reducer which manipulates the global state
+    // from every consumer component. inside the reducer, the action type will be evaluated
+    // to decide what to manipulate and how.
+    // From the consumer components we need to call this dispatch method,
+    // and send action type + payload
+    dispatch: action => this.setState(state => reducer(state, action))
+  };
+
+  componentDidMount() {
+    const proxy = process.env.REACT_APP_MY_CORS_ANYWHERE_PROXY;
+    const rootURL = process.env.REACT_APP_API_ROOT_URL;
+    const apiMethod =
+      "chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1";
+    axios
+      .get(
+        proxy + rootURL + apiMethod + `&apikey=${process.env.REACT_APP_MM_KEY}`,
+        { crossdomain: true }
+      )
+      .then(res => {
+        //console.log(res.data);
+        this.setState({ searchingFinished: true, track_list: res.data.message.body.track_list });
+      })
+      .catch(err => console.log(err));
+  }
+
+  // we can access whatever state we put in this provider as long
+  // as we use a consumer
+  render() {
+    return (
+      <Context.Provider value={this.state}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+
+export const Consumer = Context.Consumer;
